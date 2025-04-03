9d8bf1b2bdde05ebc1c3f65b3b245fdf97b884bf
Dmitry Bulankin <swed88@mail.ru>
2025-04-03 09:49:24 +0300

    fix: updated imports

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { AppProviders } from '@/app/providers/providers'
-import { App } from './app'
+import { App } from '@/app/app'
 import '@/app/styles/reset.sass'
 
 createRoot(document.getElementById('root')!).render(
